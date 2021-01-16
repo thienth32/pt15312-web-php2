ab80a8e51f71218fc262c93d58205027a07f8806
dzephyrus <57651733+dzephyrus@users.noreply.github.com>
2021-01-17 00:04:17 +0700

    up

--- demo_oop/User.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once './BaseModel.php';
-
-class User extends BaseModel
-{
-    protected $tableName = 'users';
-}
--- demo_oop/users/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../User.php';
-
-// hiển thị danh sách user trong db
-$users = User::all();
-var_dump($users);
