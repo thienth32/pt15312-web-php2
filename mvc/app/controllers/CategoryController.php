3ac1aa30a912b7388055bebb2994cdc0013ecd5c
dzephyrus <57651733+dzephyrus@users.noreply.github.com>
2021-01-23 00:00:28 +0700

    up

--- mvc/app/controllers/CategoryController.php
+++ mvc/app/controllers/CategoryController.php
@@ -1,36 +1,58 @@
 <?php
-
 namespace App\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 
-class CategoryController
-{
-    public function index()
-    {
-        return " danh sách danh mục";
-    }
-    public function editCate()
-    {
+
+class CategoryController{
+
+    public function editCate(){
         $id = $_GET['id'];
         $model = Category::find($id);
-        if ($model) {
+        if($model){
             include_once './app/views/cates/edit.php';
-        } else {
+        }else {
             header('location: ./');
         }
     }
-    public function addNew()
-    {
-        # code...
-        include_once './mvc/app/views/cate/add-new.php';
+
+    public function addNew(){
+
+        include_once './app/views/cates/add-new.php';
     }
-    public function saveCate()
-    {
+
+    public function saveCate(){
         $model = new Category();
         $model->fill($_POST);
 
         $model->save();
         header('location: ./');
     }
+
+    public function saveEdit(){
+        $id = $_GET['id'];
+        $model = Category::find($id);
+        if(!$model){
+            header('location: ./');
+            die;
+        }
+
+        $model->fill($_POST);
+        $model->save();
+        header('location: ./');
+    }
+
+    public function removeCate(){
+        $id = $_GET['id'];
+        $model = Category::find($id);
+        if(!$model){
+            header('location: ./');
+            die;
+        }
+        Product::where('cate_id', $id)->delete();
+        $model->delete();
+        header('location: ./');
+    }
+
 }
