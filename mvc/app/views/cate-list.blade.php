3e49bc2a88031a6aadf9e5806cfb359f5de881a6
thienth32 <thienth32@gmail.com>
2021-01-24 10:59:24 +0700

    sử dụng layout

--- mvc/app/views/cate-list.blade.php
+++ mvc/app/views/cate-list.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('title', 'Danh sách danh mục')
+@section('main-content')
 <table>
     <thead>
         <th>ID</th>
@@ -30,5 +25,5 @@
             </tr>
         @endforeach
     </tbody>
-</body>
-</html>
+</table>
+@endsection
