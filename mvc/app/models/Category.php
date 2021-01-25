2da3083a24355ddebed5704f24942e16da0544ff
dzephyrus <57651733+dzephyrus@users.noreply.github.com>
2021-01-25 09:40:12 +0700

    up

--- mvc/app/models/Category.php
+++ mvc/app/models/Category.php
@@ -1,8 +1,10 @@
 <?php
-
 namespace App\Models;
-
-class Category extends BaseModel
-{
-    protected $tableName = 'categories';
+use Illuminate\Database\Eloquent\Model;
+class Category extends Model{
+    protected $table = 'categories';
+    protected $fillable = ['cate_name', 'desc', 'show_menu'];
+    protected $attributes = [
+        'show_menu' => 0,
+    ];
 }
