4377966ee7871b12624aaa6f6fd6bfa43912cdb7
dzephyrus <57651733+dzephyrus@users.noreply.github.com>
2021-01-29 11:15:24 +0700

    Revert "up"
    This reverts commit 668d03a17aeb83446377ffdd75637802016b10d8.

--- mvc/app/controllers/HomeController.php
+++ mvc/app/controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 
 use App\Models\Category;
-use App\Models\User;
 
 class HomeController extends BaseController
 {
@@ -18,42 +17,6 @@ class HomeController extends BaseController
         }
         $cates->load('products');
         $this->render('cate-list', compact('cates'));
-    }
-    public function loginForm()
-    {
-        $this->render('auth.login');
-        // return "trang login ở đây";
-    }
-    public function postLogin()
-    {
-        $email = isset($_POST['email']) == true ? trim($_POST['email']) : "";
-        $password = isset($_POST['password']) == true ? trim($_POST['password']) : "";
-
-        if (empty($email) || empty($password)) {
-            header('location: ' . BASE_URL . 'login?msg=tài khoản / mật khẩu ko hợp lệ');
-        }
-        $user = User::where('email', $email)->first();
-
-        if (empty($user) || !password_verify($password, $user->password)) {
-            header('location: ' . BASE_URL . 'login?msg=tài khoản / mật khẩu ko hợp lệ');
-            die;
-        }
-
-        $_SESSION[AUTH] = [
-            'id' => $user->id,
-            'id' => $user->name,
-            'id' => $user->email,
-            'id' => $user->role,
-
-        ];
-        header('location: ' . BASE_URL);
-        // return "trang login ở đây";
-    }
-    public function logout()
-    {
-        unset($_SESSION[AUTH]);
-        header('location: ' . BASE_URL . 'login');
-        // return "trang login ở đây";
     }
 
     public function about()
--- mvc/index.php
+++ mvc/index.php
@@ -7,8 +7,6 @@ require_once './commons/utils.php';
 require_once './vendor/autoload.php';
 require_once './config/database.php';
 
-// var_dump(password_hash("123456", PASSWORD_DEFAULT));
-// die;
 // composer require illuminate/database
 // composer require illuminate/events
 
@@ -23,7 +21,6 @@ switch ($url) {
         $ctr->index();
         break;
     case "new-cate":
-        checkAuth();
         $ctr = new CategoryController();
         $ctr->addNew();
         break;
@@ -44,16 +41,8 @@ switch ($url) {
         $ctr->index();
         break;
     case "login":
-        $ctr = new HomeController();
-        $ctr->loginForm();
-        break;
-    case "post-login":
-        $ctr = new HomeController();
-        $ctr->postLogin();
-        break;
-    case "logout":
-        $ctr = new HomeController();
-        $ctr->logout();
+        $ctr = new UserController();
+        $ctr->index();
         break;
     case "remove-cate":
         $ctr = new CategoryController();
@@ -63,3 +52,48 @@ switch ($url) {
         echo "Not found!";
         break;
 }
+
+?>
+
+
+$url = isset($_GET['url']) == true ? $_GET['url'] : "/";
+// echo $_GET['url'];
+// require_once './app/controllers/HomeController.php';
+// require_once './app/controllers/ProductController.php';
+// require_once './app/models/BaseModel.php';
+// require_once './app/models/Category.php';
+
+
+<!-- require_once './commons/uitls.php';
+require_once './vendor/autoload.php';
+require_once './config/database.php';
+
+use App\Controllers\ProductController;
+use App\Controllers\HomeController;
+use App\Controllers\CategoryController;
+
+switch ($url) {
+    case "/":
+        $ctr = new HomeController();
+        echo $ctr->index();
+        break;
+    case "about":
+        $ctr = new HomeController();
+        echo $ctr->about();
+        break;
+    case "chi-tiet":
+        $ctr = new ProductController();
+        echo $ctr->detail();
+        break;
+    case "new-cate":
+        $ctr = new CategoryController();
+        echo $ctr->addNew();
+        break;
+    case "save-cate":
+        $ctr = new CategoryController();
+        echo $ctr->saveCate();
+        break;
+    default:
+        echo "not found =))))";
+        break;
+} -->
