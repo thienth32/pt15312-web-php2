def4a52d816e21e919542f438cd0823863e8ef93
dzephyrus <57651733+dzephyrus@users.noreply.github.com>
2021-02-28 16:11:00 +0700

    up

---
+++ mvc/storage/326dff4603e3ffe053472f0df8b5d21465809405.php
@@ -0,0 +1,215 @@
+<header class="header bgc-white header-type-1">
+    <div class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 order-2 col-sm-12 order-sm-2 col-md-8 order-md-1 align-self-center">
+                    <p>Mid-season sale up to 20% OFF. Use code "SALEOFF20"</p>
+                </div>
+                <div class="col-12 order-1 col-sm-12 order-sm-1 col-md-4 order-md-2">
+                    <ul class="list-inline">
+                        <li class="currency list-inline-item">
+                            <div class="btn-group">
+                                <button class="btn-link dropdown-toggle"> USD $<i class="fa fa-angle-down"></i></button>
+                                <div class="dropdown-menu">
+                                    <ul>
+                                       <li><a href="#">Euro €</a></li>
+                                       <li><a href="#" class="current">USD $</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="language list-inline-item">
+                            <div class="btn-group">
+                                <button class="btn-link dropdown-toggle">English <i class="fa fa-angle-down"></i></button>
+                                <div class="dropdown-menu">
+                                    <ul>
+                                       <li><a href="#" class="current"><img src="<?php echo e(PUBLIC_URL . 'assets/images/icons/en-gb.png'); ?>" alt="Icons"> English</a></li>
+                                       <li><a href="#"><img src="<?php echo e(PUBLIC_URL . 'assets/images/icons/fr-fr.png'); ?>" alt="Icons"> Français</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div> <!-- end of container -->
+    </div> <!-- end of top-bar -->
+
+    <div class="header-area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-6 order-2 col-sm-6 order-sm-2 col-md-4 order-md-1 col-lg-3">
+                    <div class="header-search-area">
+                        <form action="#">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search entire store here ...">
+                                <div class="input-group-append">
+                                    <button class="header-search-btn" type="submit"><i class="pe-7s-search"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-12 order-1 col-sm-12 order-sm-1 col-md-4 order-md-2 col-lg-6">
+                    <div class="logo">
+                        <a href="index.html"><img src="<?php echo e(PUBLIC_URL . 'assets/images/logo.png'); ?>" alt="Logo" class="img-fluid"></a>
+                    </div>
+                </div>
+                <div class="col-6 order-3 col-sm-6 col-md-4 col-lg-3">
+                    <div class="header-cart-area">
+                        <div class="header-cart">
+                            <div class="btn-group">
+                                <button class="btn-link dropdown-toggle icon-cart">
+                                    <i class="pe-7s-shopbag"></i>
+                                    <span class="count-style">2</span>
+                                </button>
+                                <div class="dropdown-menu">
+                                    <div class="shopping-cart-content">
+                                        <ul class="list-unstyled">
+                                            <li class="single-cart-item media">
+                                                <div class="shopping-cart-img mr-4">
+                                                    <a href="#">
+                                                        <img class="img-fluid" alt="Cart Item" src="<?php echo e(PUBLIC_URL . 'assets/images/cart/cart-1.jpg'); ?>">
+                                                        <span class="product-quantity">1x</span>
+                                                    </a>
+                                                </div>
+                                                <div class="shopping-cart-title media-body">
+                                                    <h4><a href="#">Rival Field Messenger</a></h4>
+                                                    <p class="cart-price">$120.00</p>
+                                                    <div class="product-attr">
+                                                        <span>Size: S</span>
+                                                        <span>Color: Black</span>
+                                                    </div>
+                                                </div>
+                                                <div class="shopping-cart-delete">
+                                                    <a href="#"><i class="ion ion-md-close"></i></a>
+                                                </div>
+                                            </li>
+                                            <li class="single-cart-item media">
+                                                <div class="shopping-cart-img mr-4">
+                                                    <a href="#">
+                                                        <img class="img-fluid" alt="Cart Item" src="<?php echo e(PUBLIC_URL . 'assets/images/cart/cart-2.jpg'); ?>">
+                                                        <span class="product-quantity">2x</span>
+                                                    </a>
+                                                </div>
+                                                <div class="shopping-cart-title media-body">
+                                                    <h4><a href="#">Fusion Backpack</a></h4>
+                                                    <p class="cart-price">$200.00</p>
+                                                    <div class="product-attr">
+                                                        <span>Color: White</span>
+                                                        <span>Accessories: Yes</span>
+                                                    </div>
+                                                </div>
+                                                <div class="shopping-cart-delete">
+                                                    <a href="#"><i class="ion ion-md-close"></i></a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="shopping-cart-total">
+                                            <h4>Sub-Total : <span>$320.00</span></h4>
+                                            <h4>Total : <span>$320.00</span></h4>
+                                        </div>
+                                        <div class="shopping-cart-btn">
+                                            <a class="default-btn" href="cart.html">view cart</a>
+                                            <a class="default-btn" href="checkout.html">checkout</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <ul class="list-inline">
+                            <li class="top-links list-inline-item">
+                                <div class="btn-group">
+                                    <button class="btn-link dropdown-toggle"><i class="pe-7s-config"></i></button>
+                                    <div class="dropdown-menu">
+                                        <ul>
+                                           <li><a href="register.html">Register</a></li>
+                                           <li><a href="login.html">Login</a></li>
+                                           <li><a href="my-account.html">My Account</a></li>
+                                           <li><a href="wishlist.html">Wishlist</a></li>
+                                           <li><a href="checkout.html">Checkout</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of header-cart-area -->
+                </div>
+            </div>
+        </div> <!-- end of container -->
+    </div> <!-- end of header-top -->
+
+
+
+    <!-- Start of Main and Mobile Navigation -->
+    <div class="main-nav-area bgc-white">
+        <div class="container">
+            <nav id="main_nav" class="stellarnav">
+                <ul>
+                    <li><a href="index.html"><span>Home</span></a>
+                        <ul>
+                            <li><a href="index.html">Home Page 1</a></li>
+                            <li><a href="index-2.html">Home Page 2</a></li>
+                            <li><a href="index-3.html">Home Page 3</a></li>
+                            <li><a href="index-4.html">Home Page 4</a></li>
+                            <li><a href="index-5.html">Home Page 5</a></li>
+                        </ul>
+                    </li>
+                    <li class="mega" data-columns="4"><a href="shop-grid.html"><span>Shop</span></a>
+                        <ul class="mega-container">
+                            <li><a href="#" class="mega-menu-title"><h3>Shop pages 01</h3></a>
+                                <ul>
+                                    <li><a href="shop-grid.html">Shop Grid</a></li>
+                                    <li><a href="shop-list.html">Shop List</a></li>
+                                    <li><a href="shop-right-sidebar.html">Shop Right Sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="mega-menu-title"><h3>Shop pages 02</h3></a>
+                                <ul>
+                                    <li><a href="shop-fullwidth.html">Shop Full Width</a></li>
+                                    <li><a href="single-product.html">Single Product Page</a></li>
+                                    <li><a href="compare.html">Product Compare</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="mega-menu-title"><h3>Shop pages 03</h3></a>
+                                <ul>
+                                    <li><a href="cart.html">Cart Page</a><li>
+                                    <li><a href="checkout.html">Checkout Page</a></li>
+                                    <li><a href="wishlist.html">Wishlist Page</a></li>
+                                </ul>
+                            </li>
+                            <li class="last"><a href="#" class="mega-menu-title"><h3>Shop pages 04</h3></a>
+                                <ul>
+                                    <li><a href="my-account.html">My Account</a></li>
+                                    <li><a href="login.html">Login</a></li>
+                                    <li><a href="register.html">Register</a></li>
+                                </ul>
+                            </li>
+                            <li class="fullwidth-banner">
+                                <a href="#"><img src="<?php echo e(PUBLIC_URL . 'assets/images/banners/menu-banner.jpg'); ?>" alt="Menu Banner"></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="blog.html"><span>Blog Pages</span></a>
+                        <ul>
+                            <li><a href="blog.html">Blog Full Width</a></li>
+                            <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                            <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                            <li><a href="blog-details.html">Blog Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="faqs.html"><span>Utility Pages</span></a>
+                        <ul>
+                            <li><a href="faqs.html">FAQs Page</a></li>
+                            <li><a href="404.html">404 Page</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="about.html"><span>About Us</span></a></li>
+                    <li><a href="contact.html"><span>Contact Us</span></a></li>
+                </ul>
+            </nav>
+        </div> <!-- end of container -->
+    </div>
+    <!-- End of Main and Mobile Navigation -->
+</header><?php /**PATH E:\xampp\htdocs\pt15312-web-php2\mvc\app\views/layouts/client/header.blade.php ENDPATH**/ ?>
